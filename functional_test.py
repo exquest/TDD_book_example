867a99c676821a12ff23b93214c624ad973e71a4
exquest <anthony.ainsworth@gmail.com>
2014-10-22 12:32:18 -0700

    made changes to functional test and get initial list to display - http://chimera.labs.oreilly.com/books/1234000000754/ch05.html#_three_strikes_and_refactor

--- functional_test.py
+++ functional_test.py
@@ -35,21 +35,23 @@ class NewVisitorsTest(unittest.TestCase):
 		# 1: Buy peacock feathers as an item in the todo list
 		inputbox.send_keys(Keys.ENTER)
 		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item didn't appear in table'"
-		)
-
-
 		# There is still another text box inviting her to add another item.  She
 		# enters "Use peacock feathers to make a fly" 
-		self.fail('finish the test')
-
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		# She presses enter and now shows both items on her list
-
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
+		)
+		
+		self.fail('finish the test')
+		
 		# Edith wonders if the site will remeber her list.  She sees that the
 		# site has created a unique URL for her -- there is some explanation text
 
