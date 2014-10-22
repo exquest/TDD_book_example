513b2aed2bb391f9bdfe2bcef6da59dbbec5f89f
exquest <anthony.ainsworth@gmail.com>
2014-10-21 21:10:03 -0700

    functional tests if we can input a todo item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -19,17 +20,32 @@ class NewVisitorsTest(unittest.TestCase):
 		
 		# She notices that the title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('finish the test')
 
 		# She is invited to enter a todo item right away
-
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		
 		# She types "Buy peacock feathers" into a text box
-
+		inputbox.send_keys("Buy peacock feathers")
+		
 		# When she hits enter the page updates and now the page lists:
 		# 1: Buy peacock feathers as an item in the todo list
+		inputbox.send_keys(keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		row = table.find_elements_by_tag_name('tr')
+		assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
 
 		# There is still another text box inviting her to add another item.  She
 		# enters "Use peacock feathers to make a fly" 
+		self.fail('finish the test')
+
 
 		# She presses enter and now shows both items on her list
 
