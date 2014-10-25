169a3d87af3f67413f5f73bbb021035d66ddbc4e
exquest <anthony.ainsworth@gmail.com>
2014-10-25 11:37:56 -0700

    added more users and test to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,9 +35,10 @@ class NewVisitorsTest(LiveServerTestCase):
 		inputbox.send_keys("Buy peacock feathers")
 		
 		# When she hits enter the page updates and now the page lists:
-		# 1: Buy peacock feathers as an item in the todo list
+		# 1: Buy peacock feathers as an item in the todo list table
 		inputbox.send_keys(Keys.ENTER)
-		
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
 		# There is still another text box inviting her to add another item.  She
@@ -52,12 +53,41 @@ class NewVisitorsTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		
+		# the page updates again and now shows both items on her list
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		
+		# now a new user Francis comes by to use the site
+		
+		## we use a new browser session to make sure nothing is coming over
+		## from the last set of tests
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		
+		# Francis visits the website and there is no sign of Edith's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+		
+		# Francis starts a new list by entering an item into a list
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		
+		# Francis get his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+		
+		# Again, there is no trace of Edith's list
+		page_text = self.browser.find_element_by_id_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+		
+		# Satisfied they both go to sleep
+		
 		self.fail('finish the test')
 		
-		# Edith wonders if the site will remeber her list.  She sees that the
-		# site has created a unique URL for her -- there is some explanation text
 
-		# She visits the URL and the list is there
-
-		# Satisfied she goes to sleep
 		
